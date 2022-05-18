4e493fb6ea022276079d5c1797f9005b55f0dfd7
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-05-18 23:02:19 +0200

    basic types, tuples, union, enum, objects and type assertion

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,49 @@
 "use strict";
+//Basic Types
 let id = 5;
+let company = "Orange";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
+//Tuple
+let person = [1, "Brad", true];
+//Tuple array
+let employee;
+employee = [
+    [1, "Brad"],
+    [2, "John"],
+    [3, "Jill"]
+];
+//Union
+let pid = 22;
+pid = "22";
+//Enum
+var direction1;
+(function (direction1) {
+    direction1[direction1["Up"] = 0] = "Up";
+    direction1[direction1["Down"] = 1] = "Down";
+    direction1[direction1["Left"] = 2] = "Left";
+    direction1[direction1["Right"] = 3] = "Right";
+})(direction1 || (direction1 = {}));
+console.log(direction1.Up); // should return 0
+// I can also set the values to strings
+var direction2;
+(function (direction2) {
+    direction2["Up"] = "Up";
+    direction2["Down"] = "Down";
+    direction2["Left"] = "Left";
+    direction2["Right"] = "Right";
+})(direction2 || (direction2 = {}));
+console.log(direction2.Up); //should return "Up"
+//Objects
+const user = {
+    id: 1,
+    name: "John"
+};
+const user1 = {
+    id: 1,
+    name: "John"
+};
+//Type Assertion
+//two ways
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,72 @@
+//Basic Types
 let id: number = 5
+let company: string = "Orange"
+let isPublished: boolean = true
+let x: any = "Hello"
+
+let ids: number[] = [1,2,3,4,5]
+let arr: any[] = [1,true,"Hello"]
+
+//Tuple
+let person: [number, string, boolean] = [1,"Brad",true]
+
+//Tuple array
+let employee: [number, string][]
+employee = [
+    [1,"Brad"],
+    [2,"John"],
+    [3,"Jill"]
+]
+
+//Union
+let pid: string | number = 22
+pid = "22"
+
+//Enum
+enum direction1 {
+    Up, //Every position will return a number, starting from 0 in this case by default, but I can change it setting Up = 1, and the rest will change as well
+    Down,
+    Left,
+    Right
+}
+
+console.log(direction1.Up) // should return 0
+
+// I can also set the values to strings
+
+enum direction2 {
+    Up = "Up", //Every position will return a number, starting from 0 in this case by default, but I can change it setting Up = 1, and the rest will change as well
+    Down = "Down",
+    Left = "Left",
+    Right = "Right"
+}
+
+console.log(direction2.Up) //should return "Up"
+
+//Objects
+const user: {
+    id: number,
+    name: string
+} = {
+    id: 1,
+    name: "John"
+}
+
+//Other way to set the object's type
+
+type User = {
+    id: number,
+    name: string
+}
+
+const user1: User = {
+    id: 1,
+    name: "John"
+}
+
+//Type Assertion
+//two ways
+let cid: any = 1
+// let costumerId = <number>cid // I'm setting costumerId to cid but as a diferent type(number)}
+let costumerId = cid as number // other way
 
